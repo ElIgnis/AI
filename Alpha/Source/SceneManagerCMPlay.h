d783911fef8f5ad95ec75586e7a81a12c4d9ec62
Princeton Chew <PrincetonCYK@hotmail.com>
2015-11-29 05:03:09 +0900

    forgot to push this shit

--- Alpha/Source/SceneManagerCMPlay.h
+++ Alpha/Source/SceneManagerCMPlay.h
@@ -4,6 +4,13 @@
 #include "SceneManagerGameplay.h"
 #include "SceneNode.h"
 #include "DeliveryMan.h"
+#include "Customer.h"
+
+struct CustomerShopWaypoints
+{
+	string name;
+	Vector2 waypoint;
+};
 
 class SceneManagerCMPlay : public SceneManagerGameplay
 {
@@ -25,12 +32,17 @@ public:
 	void RenderBG();
 	void RenderStaticObject();
 	void RenderMobileObject();
+	void RenderWaypoints();	//Render waypoints for debug purposes
+
+	void FetchCustomer();	//Set new customer at spawn
 
 private:
 	const float m_fBGpos_MAX_y = 730.f;
 	const float m_fBGpos_MIN_y = 310.f;
 	const float m_fBGscroll_speed = 350.f;
 	float m_fBGpos_y;
+	float m_fInputDelay;
+	const float m_fMAX_DELAY = 0.15f;
 
 	//Probability stuff
 	int m_iWeather;
@@ -40,6 +52,11 @@ private:
 	int m_iWorldTime;
 	float m_fMinutes;
 	DeliveryMan* m_dmDeliveryGuy;
+
+	vector<Customer*> m_cCustomerList;	//List containing all customers
+
+	vector<Vector2> m_v2CustomerWaypointsOUTDOOR;	//List containing all Outdoor waypoints for customer in scene
+	vector<Vector2> m_v2CustomerWaypointsINDOOR;	//List containing all Indoor waypoints for customer in scene
 };
 
 #endif
